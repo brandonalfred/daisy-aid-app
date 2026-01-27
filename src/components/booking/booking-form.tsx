5cb5f5c3bbb56effdb30d450b55a85f5d351137a
Brandon Alfred <brandon.alfred@joinforma.com>
2026-01-27 17:01:18 -0600

    fix: clear date selection when navigating back from time step

--- src/components/booking/booking-form.tsx
+++ src/components/booking/booking-form.tsx
@@ -188,6 +188,7 @@ export function BookingForm() {
           <button
             type="button"
             onClick={() => {
+              setSelectedDate(null);
               setSelectedSlot(null);
               setStep('date');
             }}
